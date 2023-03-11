294001dc4052e33da12876a365f6f621fb9aeba7
Alvalen <alvalen.shafel04@gmail.com>
2023-03-11 16:57:24 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import menu
+
+pygame.init()
+pygame.font.init()
+
+
+
+
+if __name__ == '__main__':
+    # Set up the screen
+    screen = pygame.display.set_mode((800, 600))
+
+    # Show the loading screen
+    loading_screen = menu.LoadingScreen(screen)
+    loading_screen.run()
+
+    # Show the start menu
+    start_menu = menu.StartMenu(screen)
+    start_menu.run()
